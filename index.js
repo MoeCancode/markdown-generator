9ba020096543aa7e345f837c9cf8358fb3e1a066
M A Razvi <abuzarflw@gmail.com>
2022-04-21 00:40:14 -0700

    Write markdown content inside template literal

--- index.js
+++ index.js
@@ -1,146 +1,150 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-const genMDjs = require('./utils/generateMarkdown.js')
+const fs = require("fs");
+const inquirer = require("inquirer");
+const genMDjs = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
-const questions = ["ENTER PROJECT TITLE:", "ENTER PROJECT DESCRIPTION", "ENTER INSTALLATION INSTRUCTIONS", "ENTER USAGE INFORMATION", "ENTER CONTRIBUTION GUIDELINES", "ENTER TEST INSTRUCTIONS", "CHOOSE A LICENSE", "ENTER YOUR GITHUB USERNAME", "ENTER YOUR EMAIL ADDRESS"];
+const questions = [
+  "ENTER PROJECT TITLE:",
+  "ENTER PROJECT DESCRIPTION",
+  "ENTER INSTALLATION INSTRUCTIONS",
+  "ENTER USAGE INFORMATION",
+  "ENTER CONTRIBUTION GUIDELINES",
+  "ENTER TEST INSTRUCTIONS",
+  "CHOOSE A LICENSE",
+  "ENTER YOUR GITHUB USERNAME",
+  "ENTER YOUR EMAIL ADDRESS",
+];
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: questions[0],
+        name: "title",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER A VALID TITLE");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[1],
+        name: "projectDescription",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER A VALID DESCRIPTION");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[2],
+        name: "installation",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER VALID INSTRUCTIONS");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[3],
+        name: "usage",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER VALID INSTRUCTIONS");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[4],
+        name: "contribution",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER ALL CONTRIBUTOR NAMES");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[5],
+        name: "testing",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER A GUIDE FOR TESTING");
+            return false;
+          }
+        },
+      },
+      {
+        type: "list",
+        message: questions[6],
+        name: "license",
+        choices: [
+          "MIT",
+          "Boost software license 1.0",
+          "APACHE 2.0",
+          "Eclipse Public License 1.0",
+          "No license",
+        ],
+      },
+      {
+        type: "input",
+        message: questions[7],
+        name: "git",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER A VALID USERNAME");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        message: questions[8],
+        name: "email",
+        validate: (inputvalue) => {
+          if (inputvalue) {
+            return true;
+          } else {
+            console.log(" PLEASE ENTER A VALID EMAIL");
+            return false;
+          }
+        },
+      },
+    ])
+    .then(function (answers) {
+      console.log(answers);
+      renderLicenseBadge(answers);
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt([
-        {
-            type : 'input',
-            message : questions[0],
-            name : "title",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER A VALID TITLE");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'input',
-            message : questions[1],
-            name : "projectDescription",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER A VALID DESCRIPTION");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'input',
-            message : questions[2],
-            name : "installation",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER VALID INSTRUCTIONS");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'input',
-            message : questions[3],
-            name : "usage",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER VALID INSTRUCTIONS");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'input',
-            message : questions[4],
-            name : "contribution",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER ALL CONTRIBUTOR NAMES");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'input',
-            message : questions[5],
-            name : "testing",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER A GUIDE FOR TESTING");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'list',
-            message : questions[6],
-            name : "license",
-            choices : [ "MIT", "Boost software license 1.0", "APACHE 2.0", "Eclipse Public License 1.0", "No license"],
-        },
-        {
-            type : 'input',
-            message : questions[7],
-            name : "git",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER A VALID USERNAME");
-                    return false;
-                }
-
-            }
-        },
-        {
-            type : 'input',
-            message : questions[8],
-            name : "email",
-            validate: inputvalue => {
-                if(inputvalue) {
-                    return true;
-                }
-                else {
-                    console.log(" PLEASE ENTER A VALID EMAIL");
-                    return false;
-                }
-
-            }
-        }
-    ]).then(function(message) {
-        console.log(message);
-    })
-}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(licenseObject) {
-
+  return 1;
 }
 
 // TODO: Create a function that returns the license link
@@ -15,7 +15,53 @@ function renderLicenseSection(licenseObject) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ![Github licence](http://img.shields.io/badge/license-${data.license}-blue.svg)
 
+  <br>
+  
+  ## Description 
+    ${data.description}
+
+    <br>
+
+  ## Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+
+  <br>
+
+  ## Installation 
+    ${data.install}
+
+  <br>
+
+  ## Usage 
+   ${data.usage}
+
+  <br>
+
+  ## License 
+   ${data.license}
+
+  <br>
+
+  ## Contributing 
+  ${data.contributors}
+
+  <br>
+
+  ## Tests
+  ${data.test}
+
+  <br>
+
+  ## Questions
+  If you have any questions about this projects, you can contact me directly at ${data.email}. You can find other interesting peojects that I have worked on at https://github.com/${data.github}.
 `;
 }
 
