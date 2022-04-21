d246757953bfa7b2c2aa4f7c60c0229ffe1a33ba
M A Razvi <abuzarflw@gmail.com>
2022-04-20 22:06:46 -0700

    Fill questions array with placeholder values. Call on prompt and check to see if it works

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Hi, what's your name?", "How are you?", "Can I get you a glass of water?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type : 'input',
+            message : "Hi, what's your name?",
+            name : "myName", 
+        },
+        {
+            type : 'input',
+            message : questions[1],
+            name : "haalHavaal",
+        },
+        {
+            type : 'list',
+            message : "Can I get you a glass of water?",
+            name : "isWater",
+            choices : [ "yes", "no", "maybe"]
+        }
+    ])
+}
 
 // Function call to initialize app
 init();
